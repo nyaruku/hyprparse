91b9f9d8bd63c9f4607f3efcb06066952eb51bbc
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-02-28 02:57:53 +0100

    get current user's home directory, include json.hpp

--- hyprparse.h
+++ hyprparse.h
@@ -1,5 +1,36 @@
-#include <iostream>
+get #pragma once
+#include <map>
+#include <vector>
+#include <pwd.h>
+#include <unistd.h>
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+#include <hyprparse/json.hpp>
+
+namespace hyprparse {
+    nlohmann::json j;
+    namespace helper {
+        inline std::vector<std::string> configLocations = {
+            ".config/hypr/hyprland.conf"
+        };
+
+        // Get the current user's home directory
+        inline std::string getUserDir() {
+            return std::string(getpwuid(geteuid())->pw_dir) + "/";
+        }
+    }
+    namespace debug {
+        void printJson() {
+
+        }
+    }
+    namespace cfg {
+        // Write hyprland.conf
+        bool write() {
+
+        }
+        // Read  hyprland.conf
+        bool read() {
+
+        }
+    }
 }
